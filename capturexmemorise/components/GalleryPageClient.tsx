06629439c37581606aaa6dffad58f0458ff93ca1
sumitnjmsingh <sumitsamsingh1111@gmail.com>
2025-06-28 17:13:11 +0530

    adjust ui for share button

--- capturexmemorise/components/GalleryPageClient.tsx
+++ capturexmemorise/components/GalleryPageClient.tsx
@@ -133,7 +133,7 @@ export default function GalleryPageClient({
                     })
                     .catch((err) => console.error("Share failed:", err))
                 }
-                className="absolute bottom-4 right-10 bg-gray-200 hover:bg-gray-300 text-gray-800 px-4 py-2 text-sm rounded-lg shadow-md transition flex items-center gap-1"
+                className="absolute bottom-4 right-4 bg-gray-200 hover:bg-gray-300 text-gray-800 px-4 py-2 text-sm rounded-lg shadow-md transition flex items-center gap-1"
               >
                 <Share2 className="w-4 h-4" />
                 Share
